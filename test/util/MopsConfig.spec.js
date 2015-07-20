0fac71a582f2b423a4a669f9eaba83cbe5326aa9
Dennis Kuczynski <dennis.kuczynski@gmail.com>
2015-07-20 12:33:19 -0400

    Update test

--- test/util/MopsConfig.spec.js
+++ test/util/MopsConfig.spec.js
@@ -16,6 +16,23 @@ describe('util/MopsConfig', function() {
             var username = config.get('username');
             expect(username).to.equal('myUsername');
         });
+
+        describe('when attempting to update', function() {
+            afterEach(function() {
+                config.set('username', 'myUsername');
+                config.set('secondField', undefined);
+            });
+
+            it('can add a new property', function() {
+                config.set('secondField', true);
+                expect(config.get('secondField')).to.be.true;
+            });
+
+            it('can update existing property', function() {
+                config.set('username', 'myOtherUsername');
+                expect(config.get('username')).to.equal('myOtherUsername');
+            });
+        });
     });
 
 });
